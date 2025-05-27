f9b725e3c369e8dd7198e8a5dccf3efe103dd1e5
gayathri <sgayathricse97@gmail.com>
2025-05-27 09:45:52 +0530

    updated code

--- full.js
+++ full.js
@@ -2,3 +2,15 @@ var arr=[10,20,30]
 var arr1=[...arr,40,50]
 console.log(arr)
 console.log(arr)
+const getData=async()=>{
+    try{
+        const response = await fetch('https://jsonplaceholder.typicode.com/users')
+        const users=await response.json()
+        console.log(users);
+
+    }
+    catch(error){
+        console.log('Error:${error}');
+    }
+}
+getData()
